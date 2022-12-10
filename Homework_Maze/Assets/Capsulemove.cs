bbbe4472e23e9d0ee88b8de98485106e29d51360
nimonemo434 <109446732+nimonemo434@users.noreply.github.com>
2022-12-10 15:22:44 +0900

    okay
    okay

---
+++ Homework_Maze/Assets/Capsulemove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsulemove : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 1, z);
+            }
+        }
+    }
+}
